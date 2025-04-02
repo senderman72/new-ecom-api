2d7b5f3e5164c8daeb05d6e07edfbebbc757a3a5
Erik Persson <perssonerik7@gmail.com>
2025-04-02 14:58:07 +0200

    uppdatering av clien url

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "*",
+    origin: "https://new-ecommerce-client.vercel.app/",
     credentials: true,
   })
 );
