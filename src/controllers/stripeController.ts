1ace7a9229bae618944a0c24f7bfb8b92cce1f9c
Erik Persson <perssonerik7@gmail.com>
2025-04-02 13:20:14 +0200

    fixade modeller och controllers

---
+++ src/controllers/stripeController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import { db } from "../config/db";
+import { STRIPE_SECRET_KEY } from "../constants/env";
+const stripe = require("stripe")(STRIPE_SECRET_KEY);
+
+export const checkoutSessionEmbedded = async (
+  req: Request,
+  res: Response
+) => {};
+
+export const webhook = async (req: Request, res: Response) => {};
---
+++ src/utilities/logger.ts
@@ -0,0 +1,3 @@
+export const logError = (error: unknown) => {
+  return error instanceof Error ? error.message : "Unknown error";
+};
